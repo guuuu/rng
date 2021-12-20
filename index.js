f9de934a783c13246366a4ef0411d3a3fd65caec
Gustavo Nascimento <guganascimento2000@gmail.com>
2021-12-20 04:48:40 +0000

    Fixed a bunch of errors | updated design | missing mobile design

--- index.js
+++ index.js
@@ -1,38 +1,18 @@
 $(document).ready(() => { 
-    $("#val").html("Choose amount of entries - " + $("#qtd").val()); 
     $("#rnd_for_txt").html(`Randomize for - ${$("#randomize_for").val()} seconds`); 
     $("#between_txt").html(`Time between randoms - ${$("#between").val()} seconds`);
 });
-
-$(document).on("input", "#qtd", () => {
-    let entries = $("#qtd").val();
-    $("#val").html(`Choose amount of entries - ${entries}`);
-    $("#main").empty();
-
-    let size = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--size'));
-    let area = size * 2;
-    let square_size = parseInt((area / entries) / 2);
-    document.documentElement.style.setProperty('--square_width', `${square_size}px`);
-
-    for (let i = 0; i < entries; i++) {
-        let s = document.createElement("div");
-        $(s).addClass("square");
-        $("#main").append(s);
-    }
-})
 
 $(document).on("input", "#randomize_for", () => { $("#rnd_for_txt").html(`Randomize for - ${$("#randomize_for").val()} seconds`); });
 $(document).on("input", "#between", () => { $("#between_txt").html(`Time between randoms - ${$("#between").val()} seconds`); });
 
 $("#pop_winner").click(function() {
-    if(!$(this).hasClass("checked"))
-        $(this).addClass("checked");
-    else
-        $(this).removeClass("checked");
+    if(!$(this).hasClass("checked")) $(this).addClass("checked");
+    else $(this).removeClass("checked");
 })
 
 $("#bt").click(() => {
-    let entries = $("#qtd").val();
+    let entries = $("#main").children().length;
     let run = true;
     let last = null;
     let flag = false;
@@ -42,15 +22,7 @@ $("#bt").click(() => {
     let pop_winner = $("#pop_winner").hasClass("checked");
 
     
-    if(pop_winner && $("#qtd").val() > 2){
-        $("#main").children().each(function(_, el) {
-            if($(el).hasClass("winner")){
-                $(this).remove();
-                $("#qtd").val($("#qtd").val() - 1)
-                $("#val").html(`Choose amount of entries - ${$("#qtd").val()}`);
-            }
-        });
-    }
+    if(pop_winner && $("#main").children().length > 2){ $("#main").children().each(function(_, el) { if($(el).hasClass("winner")){ $(this).remove(); } }); }
 
     $(".winner").removeClass("winner");
 
@@ -79,16 +51,30 @@ $("#entrie").keypress(function(e) { if(e.keyCode === 13){ addEntrie(String($("#e
 
 let addEntrie = (val) => {
     let stop = false;
-    $(".square").each((_, el) => {
-        if($(el).children().length === 0 && !stop){
-            let d = document.createElement("div");
-            $(d).html(val)
-            $(el).append(d);
-            stop = !stop;
-        }
-    });
+    let s = document.createElement("div");
 
-    $("#entrie").val("");
+    if($("#main").children().length < 49){
+        $(s).addClass("square");
+        $("#main").append(s);
+        
+        $(".square").each((_, el) => {
+            if($(el).children().length === 0 && !stop){
+                let d = document.createElement("div");
+                $(d).html(val)
+                $(el).append(d);
+                stop = !stop;
+            }
+        });
+    
+        $("#entrie").val("");
+    }
+    else{
+        $("#msg").html("You can't have more entries...");
+        $("#msg").removeClass("hide");
+        setTimeout(() => { $("#msg").addClass("hide"); $("#msg").html(""); }, 2500);
+    }
 }
 
 $("#add_entrie").click(() => { addEntrie(String($("#entrie").val())); })
+
+$(document).on("click", ".square", function(){ $(this).remove(); })
